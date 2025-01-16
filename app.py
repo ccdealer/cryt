e1f1928dd296d56c3bc1538557b6ba7c544b1bbe
MaratAliakhmetov <ccdealerirk@gmail.com>
2025-01-16 20:42:53 +0500

    first commit boom

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import (
+    Flask, #библиотека 
+    render_template, 
+    redirect,
+    request
+)
+from turtle import *
+
+app = Flask(__name__)
+
+
+        
+
+@app.route("/", methods = ["GET", "POST"])
+def get_home():
+    return render_template("home.html")
+
+
+@app.route("/str1", methods = ["GET", "POST"])
+def get_str1():
+    if request.method == "POST":
+        for i in range(3):
+            forward(100)
+            left(90)
+        forward(100)
+        return render_template("str1.html" )
+    return render_template("str1.html")
+
+@app.route("/str2", methods = ["GET", "POST"])
+def get_str2():
+    return render_template("str2.html")
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
